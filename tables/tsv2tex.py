7035636f3bebfdc2692fd96091628d775989fc32
Jose Garcia <garciafertson@gmail.com>
2021-09-24 17:57:22 +0200

    add citations to library.bib, include cites in table

--- tables/tsv2tex.py
+++ tables/tsv2tex.py
@@ -8,4 +8,4 @@ tsvfile = sys.argv[1]
 df = pd.read_csv(tsvfile, sep='\t')
 print(df)
 with pd.option_context("max_colwidth", 1000):
-    df.to_latex(tsvfile + ".tex",index=False, column_format='p{2cm}|p{0.6cm}|p{4cm}|p{4cm}|p{3.4cm}')
+    df.to_latex(tsvfile + ".tex",index=False, column_format='p{2cm}|p{0.6cm}|p{4cm}|p{4cm}|p{3.4cm}', escape=False )
