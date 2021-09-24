0c8ffbc4ce2444eff0ebbf9a074c366ad51be30a
theoportlock <zn.tportlock@gmail.com>
2021-09-24 11:17:34 +0200

    Fixed figure boooyaaa

--- tables/tsv2tex.py
+++ tables/tsv2tex.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 import sys
 import pandas as pd
-df = pd.read_csv(sys.argv[1], sep='\t')
+'''
+tsvfile = 'Table1_binning_software_since2017.tsv'
+'''
+tsvfile = sys.argv[1]
+df = pd.read_csv(tsvfile, sep='\t')
 print(df)
-df.to_latex(sys.argv[1] + ".tex",index=False)
+with pd.option_context("max_colwidth", 1000):
+    df.to_latex(tsvfile + ".tex",index=False, column_format='p{2cm}|p{0.6cm}|p{4cm}|p{4cm}|p{3.4cm}')
